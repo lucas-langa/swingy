c25747ae3139fa696205fe1b0eb0764e79bcab50
lucas-langa <llanga@student.wethinkcode.co.za>
2019-07-18 12:55:18 +0200

    factory is done

--- za/co/wethinkcode/HeroTest.java
+++ za/co/wethinkcode/HeroTest.java
@@ -11,21 +11,6 @@ import za.co.wethinkcode.heroes.Hero;
 public class HeroTest {
 
 	public static void main( String [] args ){
-		/* HeroBuilder flanker = new FlankHeroBuilder();
-		HeroEngineer heroEngineer = new HeroEngineer( flanker );
-		heroEngineer.makeHero( "maeve" );
-		Hero maeveFlank = heroEngineer.getHero();
-
-		HeroBuilder tank = new TankHeroBuilder();		
-		heroEngineer = new HeroEngineer( tank );
-		heroEngineer.makeHero( "Khan" );
-		Hero khanHero = heroEngineer.getHero();
-		
-		HeroBuilder damage = new DamageHeroBuilder();
-		heroEngineer = new HeroEngineer( damage );
-		heroEngineer.makeHero( "Imani" );
-		Hero lastWarder = heroEngineer.getHero(); */
-
 		Hero uHero = HeroFactory.newHero("lucas", "flank");
 		Hero vHero = HeroFactory.newHero("mpho", "damage");
 		Hero wHero = HeroFactory.newHero("kerane", "tank");
