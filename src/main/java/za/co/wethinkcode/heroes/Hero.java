329ab7ae9f3e1f96b3d5d2c0539eca8691b41edf
lucas-langa <llanga@student.wethinkcode.co.za>
2019-08-15 10:46:13 +0200

    db connects, inserts, exception handling needed.

--- src/main/java/za/co/wethinkcode/heroes/Hero.java
+++ src/main/java/za/co/wethinkcode/heroes/Hero.java
@@ -4,6 +4,8 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 
 
@@ -16,15 +18,10 @@ public class Hero implements Serializable, HeroStats {
     @Column(name="id")
     private Integer id;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getId() {
-        return id;
-    }
+    @Size(min = 3, max = 15)
 	@Column( name = "heroName")
 	private String					heroName;
+
 	@Column( name = "heroClass")
 	private String					heroClass;
 	@Column( name = "heroLevel")
@@ -37,6 +34,8 @@ public class Hero implements Serializable, HeroStats {
 	private int						heroDefense;
 	@Column( name = "heroHitPoints" )
 	private int						heroHitPoints;
+
+	public void setId(Integer id) { this.id = id;}
 
 	public void 		setHeroName( String name ){
 		this.heroName = name;
@@ -66,9 +65,43 @@ public class Hero implements Serializable, HeroStats {
 		this.heroHitPoints = hitPoints;
 	}
 
+	@NotNull
+	public Integer getId() {
+		return id;
+	}
+
 	@Override
 	public	String 		toString( ) {
-		String things = " this level " + this.heroLevel + " so much xp " + this.heroExperience + " deals " + this.heroAttack + " much damage and has this many defense points " + this.heroDefense;
-		return ( "Lo!,behold , i am " + this.heroName + " of class " + this.heroClass + " sitting at this health pool " + this.heroHitPoints + things );
+		String things = " this level " + this.getHeroLevel() + " so much xp " + this.getHeroExperience() + " deals " + this.getHeroAttack() + " much damage and has this many defense points " + this.getHeroDefense();
+		return ( "Lo!,behold , i am " + this.getHeroName() + " of class " + this.getHeroClass() + " sitting at this health pool " + this.getHeroHitPoints() + things );
+	}
+
+	@NotNull
+	public String getHeroName() {
+		return heroName;
+	}
+	@NotNull
+	public String getHeroClass() {
+		return heroClass;
+	}
+
+	public int getHeroLevel() {
+		return heroLevel;
+	}
+
+	public int getHeroExperience() {
+		return heroExperience;
+	}
+
+	public int getHeroAttack() {
+		return heroAttack;
+	}
+
+	public int getHeroDefense() {
+		return heroDefense;
+	}
+
+	public int getHeroHitPoints() {
+		return heroHitPoints;
 	}
 }
